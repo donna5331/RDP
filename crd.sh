3f7a002246f103558a87b9d466ce61a271f9ab33
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-28 10:10:55 +0530

    Create crd.sh

---
+++ crd.sh
@@ -0,0 +1,10 @@
+sudo apt update
+wget -nc https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb 
+sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+sudo apt install xfce4-terminal -y
+sudo apt install firefox-esr -y 
+sudo apt-get install geany -y
+sudo apt-get install vim-gtk3 -y
+sudo apt install iputils-ping -y
+sudo apt-get update && sudo apt-get install -y vim xterm pulseaudio cups
